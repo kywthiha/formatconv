bb3f4392efd0b3bc5a9bdfab798a63676a28f868
Kyaw Thi Ha <kyawthiha@gicjp>
2022-10-19 16:32:51 +0630

    feat:file upload with s3 commit by kyawthiha

---
+++ formatconv/src/components/FileChange/FileItem.vue
@@ -0,0 +1,164 @@
+<!--
+    クラス名 : FileUploadForm
+    概要 : 変換ファイルアップロード処理画面
+    作成者 : GICM_MHK
+    作成日 : 2022/10/17　 
+-->
+<script setup>
+import { onMounted, reactive, ref } from "vue";
+
+const props = defineProps(["item"]);
+const progress = reactive({
+  loaded: {
+    fileSize: 0,
+    fileUnit: "KB",
+  },
+  speed: {
+    fileSize: 0,
+    fileUnit: "KB",
+  },
+  percentage: 0,
+});
+const complete = ref(false);
+
+const humanFileSize = (size) => {
+  console.log(size);
+  var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+  return {
+    fileSize: (size / Math.pow(1024, i)).toLocaleString(),
+    fileUnit: ["B", "KB", "MB", "GB", "TB"][i],
+  };
+};
+
+const { fileUnit, fileSize } = humanFileSize(props.item.file.size);
+
+const uploadToS3 = async () => {
+  progress.value = 0;
+  const started_at = new Date();
+  const handlPprogress = (e) => {
+    console.log(e);
+    if (
+      e.currentTarget &&
+      e.currentTarget.readyState == 4 &&
+      e.currentTarget.status != 200
+    ) {
+      try {
+        const parser = new DOMParser();
+        const response = parser.parseFromString(
+          e.currentTarget.responseText,
+          "text/xml"
+        );
+        console.log(response);
+        const code = response.getElementsByTagName("Code")[0].textContent;
+        const message = response.getElementsByTagName("Message")[0].textContent;
+        console.log(code, message);
+        console.log(response.getElementsByTagName("Code")[0].textContent);
+        console.log(response.getElementsByTagName("Message")[0].textContent);
+      } catch (error) {
+        console.log(e.currentTarget.responseText);
+      }
+    }
+    if (e.lengthComputable) {
+      const seconds_elapsed =
+        (new Date().getTime() - started_at.getTime()) / 1000;
+      const percentage = Math.round((e.loaded / e.total) * 90) + 10;
+      progress.loaded = humanFileSize(e.loaded);
+      progress.speed = humanFileSize(
+        seconds_elapsed ? e.loaded / seconds_elapsed : 0
+      );
+      progress.percentage = percentage;
+    }
+    if (e.type == "load") {
+      complete.value = true;
+      console.log("Upload Complete");
+    }
+  };
+  console.log("Start Upload");
+  console.log(props.item.upload_url);
+  const xhr = new XMLHttpRequest();
+  xhr.upload.addEventListener("progress", handlPprogress, false);
+  xhr.addEventListener("load", handlPprogress, false);
+  xhr.addEventListener("error", handlPprogress, false);
+  xhr.addEventListener("abort", handlPprogress, false);
+  xhr.open("PUT", props.item.upload_url);
+  xhr.setRequestHeader("Content-Type", props.item.file.type);
+  xhr.send(props.item.file);
+};
+
+onMounted(() => {
+  if (props.item.file && props.item.upload_url && !complete.value) {
+    uploadToS3().then((res) => {
+      console.log(res);
+    });
+  }
+});
+</script>
+<template>
+  <div class="file-item">
+    <div class="row justify-content-between">
+      <div class="col align-self-center">
+        <div class="fw-bold">フォルダ名</div>
+        <div>{{ item.file.name }}</div>
+      </div>
+      <div class="col align-self-center">
+        <div class="fw-bold">サイズ（{{ fileUnit }}）</div>
+        <div>{{ fileSize }}</div>
+      </div>
+      <div class="col-auto align-self-center">
+        <button class="btn-upload" v-if="item.upload_url && !complete">
+          {{ progress.percentage }}%
+        </button>
+        <button class="btn-upload btn-complete" v-else-if="complete">
+          完了
+        </button>
+        <button class="btn-upload" v-else>始める</button>
+      </div>
+    </div>
+    <template v-if="item.upload_url && !complete">
+      <div class="progress mt-2 mb-1" style="height: 2px">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          aria-label="Example with label"
+          :style="{ width: `${progress.percentage}%` }"
+          :aria-valuenow="progress.percentage"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        ></div>
+      </div>
+      <div class="row justify-content-between">
+        <div class="col-auto">
+          {{ progress.loaded.fileSize }}{{ progress.loaded.fileUnit }} /
+          {{ fileSize }}{{ fileUnit }}
+        </div>
+        <div class="col-auto">
+          {{ progress.speed.fileSize }}{{ progress.speed.fileUnit }}/s
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+<style>
+.file-item {
+  border: 1px solid #787878;
+  border-radius: 0.375rem;
+  padding: 0.5rem;
+}
+
+.file-item .btn-complete {
+  background-color: #2097f3 !important;
+}
+
+.file-item .btn-upload {
+  border: none;
+  outline: none;
+  width: 7rem;
+  border-radius: 0.375rem;
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 0.375rem 0.75rem;
+  display: inline-block;
+  background-color: #8cc34a;
+  color: #ffffff;
+}
+</style>
--- formatconv/src/components/FileChange/FileUploadForm.vue
+++ formatconv/src/components/FileChange/FileUploadForm.vue
@@ -4,147 +4,131 @@
     作成者 : GICM_MHK
     作成日 : 2022/10/17　 
 -->
-<script>
+<script setup>
 import { computed } from "@vue/reactivity";
 import { ref, onBeforeMount, onBeforeUpdate } from "vue";
-import { useRouter, useRoute } from "vue-router";
+import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import FileItem from "./FileItem.vue";
 
 // const events = ["dragenter", "dragleave", "dragover", "drop"];
+const store = useStore();
+let moveMFASetting = ref(false);
+let router = useRouter();
+const files = ref([]);
+const service_name = "formatconv";
 
-export default {
-  setup() {
-    const store = useStore();
-    let fileDropSelected = ref(false);
-    let fileSelected = ref("");
-    let fileName = ref("");
-    let fileSize = ref("");
-    let moveMFASetting = ref(false);
-    let router = useRouter();
-    let route = useRoute();
-    let fileList = ref("");
-
-    function handleDrop(event) {
-      console.log("fileSelected ", event.target.files);
-      for (var i = 0; i < event.target.files.length; i++) {
-        fileList.value = event.target.files[i];
+const getUploadUrl = async (targetFiles) => {
+  console.log(targetFiles);
+  try {
+    const response = await fetch(
+      "https://wdduz75b1m.execute-api.us-east-1.amazonaws.com/test/presigned-url-upload",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: store.state.authModule.idToken,
+        },
+        body: JSON.stringify({
+          service_name,
+          file_names: [...targetFiles].map((file) => file.name),
+        }),
       }
-      console.log("fileList ", fileList.value);
-      fileDropSelected.value = true;
-      fileSelected.value = event.target.files[0];
-      fileName.value = event.target.files[0].name;
-      fileSize.value = event.target.files[0].size / Math.pow(1024, 3);
-      console.log("fileSelected ", fileSelected);
-      console.log("fileName ", fileName.value);
-      console.log("fileSize ", fileSize.value);
-    }
-
-    function dragFile(event) {
-      console.log(event.dataTransfer.files);
-      console.log("dragFile ", event.dataTransfer.files[0].name);
-      fileDropSelected.value = true;
-      fileSelected.value = event.dataTransfer.files[0];
-      fileName.value = event.dataTransfer.files[0].name;
-      fileSize.value = event.dataTransfer.files[0].size / Math.pow(1024, 3);
-      console.log("fileSelected ", fileSelected);
-      console.log("fileName ", fileName);
-      console.log("fileSize ", fileSize);
-    }
-
-    function changePassword() {
-      router.replace({
-        name: "ChangePassword",
-      });
-    }
-
-    function enableMFASetting() {
-      console.log("enableMFASetting ");
-      moveMFASetting.value = true;
-      router.replace({
-        name: "Mfa",
-      });
-    }
-
-    function setUserSessionInfo(session) {
-      setTimeout(function () {
-        store.dispatch("autoLogout");
-        router.replace("/signin");
-        alert("You have been automatically logged out");
-      }, autoTimeout(session));
-      store.dispatch("setSession", session);
-      store.dispatch("setIDToken", session.getIdToken().getJwtToken());
-      store.dispatch(
-        "setUsername",
-        session.idToken.payload["cognito:username"]
-      );
-      store.dispatch("setIsAuthenticated", true);
-      store.dispatch("setEmail", session.idToken.payload.email);
-    }
-
-    const cognitoUserName = computed(
-      () => store.state.authModule.cognitoUserName
     );
 
-    const profilename = computed(() => store.state.authModule.name);
-
-    //
-    const logout = () => {
-      store.dispatch("logout");
-      router.push({
-        name: "signin",
-        params: { message: "You have logged out" },
+    if (response.ok) {
+      const data = await response.json();
+      files.value = [...targetFiles].map((file) => {
+        return {
+          file: file,
+          ...data.find((i) => i.file_name == file.name),
+        };
       });
-    };
-
-    //　ユーザーに対して MFA が有効か無効かを格納する計算プロパティ
-    const mfaValue = computed(() => {
-      console.log(" in file upload ", store.getters.isMFAEnabled);
-      return store.getters.isMFAEnabled;
-    });
-
-    //　ログインしたユーザーの mFA の現在の状態を確認する
-    onBeforeMount(function () {
-      store.dispatch("fetchMFAValue");
-    });
-
-    onBeforeUpdate(function () {
-      store.dispatch("fetchMFAValue");
-    });
-
-    // const self = this;
-    function enableMFAStatus(event) {
-      console.log("checkbox value ", event.target.checked);
-      // this.$root.broadcast("checkedValue", {
-      //   users: event.target.checked,
-      // });
-      // self.$root.$emit("checkedValue", event.target.checked);
-      // TotpForm.$emit("checkedValue", event.target.checked);
-      router.replace({
-        name: "Mfa",
-        query: { checkedValue: event.target.checked },
-      });
+    } else {
+      const data = await response.json();
+      console.log(data);
+      alert(data.message);
     }
-
-    return {
-      changePassword,
-      handleDrop,
-      dragFile,
-      fileDropSelected,
-      fileSelected,
-      fileName,
-      fileSize,
-      File,
-      enableMFASetting,
-      moveMFASetting,
-      setUserSessionInfo,
-      profilename,
-      logout,
-      cognitoUserName,
-      enableMFAStatus,
-      mfaValue,
-    };
-  },
+  } catch (e) {
+    console.log(e);
+  }
 };
+
+async function handleDrop(event) {
+  files.value = [...event.target.files].map((file) => ({ file }));
+  await getUploadUrl(event.target.files);
+}
+async function dragFile(event) {
+  files.value = [...event.dataTransfer.files].map((file) => ({ file }));
+  await getUploadUrl(event.dataTransfer.files);
+}
+
+function changePassword() {
+  router.replace({
+    name: "ChangePassword",
+  });
+}
+
+function enableMFASetting() {
+  console.log("enableMFASetting ");
+  moveMFASetting.value = true;
+  router.replace({
+    name: "Mfa",
+  });
+}
+
+function setUserSessionInfo(session) {
+  setTimeout(function () {
+    store.dispatch("autoLogout");
+    router.replace("/signin");
+    alert("You have been automatically logged out");
+  }, autoTimeout(session));
+  store.dispatch("setSession", session);
+  store.dispatch("setIDToken", session.getIdToken().getJwtToken());
+  store.dispatch("setUsername", session.idToken.payload["cognito:username"]);
+  store.dispatch("setIsAuthenticated", true);
+  store.dispatch("setEmail", session.idToken.payload.email);
+}
+
+const profilename = computed(() => store.state.authModule.name);
+
+//
+const logout = () => {
+  store.dispatch("logout");
+  router.push({
+    name: "signin",
+    params: { message: "You have logged out" },
+  });
+};
+
+//　ユーザーに対して MFA が有効か無効かを格納する計算プロパティ
+const mfaValue = computed(() => {
+  console.log(" in file upload ", store.getters.isMFAEnabled);
+  return store.getters.isMFAEnabled;
+});
+
+//　ログインしたユーザーの mFA の現在の状態を確認する
+onBeforeMount(function () {
+  store.dispatch("fetchMFAValue");
+});
+
+onBeforeUpdate(function () {
+  store.dispatch("fetchMFAValue");
+});
+
+// const self = this;
+function enableMFAStatus(event) {
+  console.log("checkbox value ", event.target.checked);
+  // this.$root.broadcast("checkedValue", {
+  //   users: event.target.checked,
+  // });
+  // self.$root.$emit("checkedValue", event.target.checked);
+  // TotpForm.$emit("checkedValue", event.target.checked);
+  router.replace({
+    name: "Mfa",
+    query: { checkedValue: event.target.checked },
+  });
+}
 </script>
 <template>
   <div>
@@ -205,55 +189,72 @@ export default {
         <label>{{ $t("screenItemProperties.common.title") }}</label>
       </template>
     </header-display>
-    <body-display>
-      <template v-slot:body>
-        <!-- ファイルのドラッグ と ドロップエリア -->
-        <div @dragover.prevent @drop.prevent v-if="!fileDropSelected">
-          <input
-            @change="handleDrop"
-            type="file"
-            multiple
-            id="inputName"
-            class="visually-hidden"
-          />
-          <label for="inputName" class="lbl">{{
-            $t("screenItemProperties.button.dataConvertionUploadBtn")
-          }}</label>
+    <div class="file-upload-form">
+      <!-- ファイルのドラッグ と ドロップエリア -->
+      <div
+        @dragover.prevent
+        @drop.prevent
+        class="file-upload-container"
+        v-if="!files.length"
+      >
+        <input
+          @change="handleDrop"
+          type="file"
+          multiple
+          id="inputName"
+          class="visually-hidden"
+        />
+        <label for="inputName" class="lbl">{{
+          $t("screenItemProperties.button.dataConvertionUploadBtn")
+        }}</label>
 
-          <div @drop="dragFile" class="lblFile">
-            <label>+</label>
-            <div>
-              <ul v-for="file in File" :key="file">
-                <li>{{ file.name }}</li>
-              </ul>
-            </div>
-          </div>
+        <div @drop="dragFile" class="drag-container">
+          <span>+</span>
         </div>
-        <!-- ドラッグ と ドロップしたファイルを表示するエリア -->
-        <div class="lblText" v-if="fileDropSelected">
-          <table>
-            <tr>
-              <td class="filename-lbl">
-                <b>{{ $t("screenItemProperties.fileUpload.fileName") }}</b>
-              </td>
-              <td class="filesize-lbl">
-                <b>{{ $t("screenItemProperties.fileUpload.fileSize") }}</b>
-              </td>
-              <td rowspan="2" class="convert-td">
-                <button>
-                  {{ $t("screenItemProperties.button.convertingBtn") }}
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td class="filesize-lbl" style="column-width: 200px">
-                {{ fileName }}
-              </td>
-              <td class="filesize-lbl">{{ fileSize }}</td>
-            </tr>
-          </table>
+      </div>
+      <div class="file-list-container" v-if="files.length">
+        <div class="file-list">
+          <!-- ドラッグ と ドロップしたファイルを表示するエリア -->
+          <FileItem v-for="item in files" :key="item.file_name" :item="item" />
         </div>
-      </template>
-    </body-display>
+      </div>
+    </div>
   </div>
 </template>
+<style>
+.file-upload-form {
+  min-height: 50vh;
+}
+.file-upload-form .file-list-container {
+  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+}
+.file-upload-form .file-list {
+  display: flex;
+  width: 60%;
+  flex-direction: column;
+  gap: 1rem;
+}
+.file-upload-form .file-upload-container {
+  display: flex;
+  margin-top: 2rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.file-upload-form .file-upload-container .drag-container {
+  width: 15rem;
+  height: 10rem;
+  background-color: #e0e0e0;
+  border-radius: 5px;
+  border: 1px solid #4a4a4a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.file-upload-form .file-upload-container .drag-container span {
+  font-size: 1.5rem;
+}
+</style>
