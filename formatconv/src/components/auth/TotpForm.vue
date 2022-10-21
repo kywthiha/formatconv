c5ab16b84a56381bf28cc5ab60fe536d9c4cb7df
kyawzinpaing9 <kyawzinpaing@gicjp.biz>
2022-10-21 08:31:22 +0630

    add login header to totp form

--- formatconv/src/components/auth/TotpForm.vue
+++ formatconv/src/components/auth/TotpForm.vue
@@ -12,10 +12,12 @@ import useAlert from "../../hooks/alert";
 import store from "../../store/index";
 import { POOL_DATA } from "../../config/cognito";
 import { useRoute, useRouter } from "vue-router";
+import LoginHeaderForm from "../auth/LoginHeaderForm.vue";
 
 export default {
   components: {
     QrcodeVue,
+    LoginHeaderForm,
   },
   setup() {
     const route = useRoute();
@@ -29,17 +31,6 @@ export default {
     console.log("tet ", checkedValue.value);
 
     const { message, messageStyleType, setMessage } = useAlert();
-
-    //　ログインしたユーザーの mFA の現在の状態を確認する
-    onBeforeMount(function () {
-      store.dispatch("fetchMFAValue");
-    });
-
-    onBeforeUpdate(function () {
-      store.dispatch("fetchMFAValue");
-    });
-
-    const profilename = computed(() => store.state.authModule.name);
 
     // 新しい Qr コードを作成する
     function newQRCode() {
@@ -176,25 +167,11 @@ export default {
       newQRCode();
     }
 
-    function enableMFAStatus(event) {
-      console.log("enableMFAStatus ", event.target.checked);
-      // mfaValue.value = event.target.checked;
-      newQRCode();
-    }
-
     function changePassword() {
       router.replace({
         name: "ChangePassword",
       });
     }
-
-    const logout = () => {
-      store.dispatch("logout");
-      router.push({
-        name: "signin",
-        params: { message: "You have logged out" },
-      });
-    };
 
     return {
       isEnabled,
@@ -209,74 +186,15 @@ export default {
       message,
       messageStyleType,
       setMessage,
-      profilename,
       checkedValue,
-      enableMFAStatus,
       changePassword,
-      logout,
     };
   },
 };
 </script>
 <template>
   <div>
-    <header-display>
-      <template v-slot:totp-slot>
-        <button>
-          <div class="form-switch" style="padding-left: 0em">
-            <label class="form-check-label" for="flexSwitchCheckDefault">
-              {{ $t("screenItemProperties.common.mfaOnOff") }}</label
-            >
-            <input
-              class="form-check-input"
-              style="margin-left: 0em"
-              type="checkbox"
-              id="flexSwitchCheckDefault"
-              :value="mfaValue"
-              v-model="mfaValue"
-              @change="enableMFAStatus($event)"
-            />
-          </div>
-        </button>
-      </template>
-      <template v-slot:register-slot>
-        <div class="dropdown">
-          <button
-            class="btn btn-secondary dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton1"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            {{ profilename }}
-          </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li>
-              <a class="dropdown-item" href="#" @click="changePassword">{{
-                $t("screenItemProperties.changePassword.changePassword")
-              }}</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">{{
-                $t("screenItemProperties.common.serviceConfirmMenu")
-              }}</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#" @click.prevent="logout"
-                >ログアウト</a
-              >
-            </li>
-          </ul>
-        </div>
-      </template>
-      <template v-slot:titlebar-slot>
-        <div class="logo-icon">
-          <img src="../../assets/logo-icon.png" class="img-fluid" />
-        </div>
-        <!-- タイトル -->
-        <label>{{ $t("screenItemProperties.common.title") }}</label>
-      </template>
-    </header-display>
+    <login-header-form></login-header-form>
     <body-display>
       <template v-slot:body>
         <div class="row">
